97f49a1319ccb664082db18842755a4c15de33be
Ilya Kublin <raka699@gmail.com>
2022-11-04 15:30:23 +0300

    Added authentication

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using ClientSideBoard;
 
 using Microsoft.AspNetCore.Components.Web;
@@ -8,4 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ClientAuthentificationStateProvider>();
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+builder.Services.AddAuthorizationCore();
 await builder.Build().RunAsync();
---
+++ wwwroot/js/cookies.js
@@ -0,0 +1,14 @@
+﻿export function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if ((c.indexOf(name)) == 0) {
+           
+            return c.substr(name.length);
+        }
+
+    }
+    
+    return null;
+}
